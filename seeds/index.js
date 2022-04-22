c6b406c698da07f5be41118d35963b2ceaf8f25f
Jeffery Patterson <jpatterson933@ucla.edu>
2022-04-22 13:53:03 -0700

    updated now running live

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,15 @@
 const sequelize = require('../config/connection');
 const { Cart, Users } = require('../models');
-const Products = require('../models/products');
+// const Products = require('../models/products');
 const Heroes = require('../models/heroes');
 const heroData = require('./heroes.json');
-const productData = require('./products.json');
+// const productData = require('./products.json');
 const userData = require('./user.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  // seed database with our users
   await Users.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
@@ -19,10 +21,10 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  await Products.bulkCreate(productData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await Products.bulkCreate(productData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
   process.exit(0);
 };
 seedDatabase();
